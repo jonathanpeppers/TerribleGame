eec74bd89c87449be728d1ced43d78e27073fd40
Jonathan Peppers <jonathan.peppers@gmail.com>
2016-02-19 15:27:40 -0600

    Updating batch file

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 @echo off
-"C:\Program Files\Unity\Unity.exe" -quit -batchmode -logFile -executeMethod BuildScript.Android
+.nuget\NuGet.exe install FAKE -Version 4.20.0
+packages\FAKE.4.20.0\tools\FAKE.exe build.fsx %1
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#nuget install FAKE -Version 4.20.0
+nuget install FAKE -Version 4.20.0
 mono --runtime=v4.0 packages/FAKE.4.20.0/tools/FAKE.exe build.fsx $@
