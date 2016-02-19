e9d0b27d91973a28ade37221cd4574359f7d3159
Jonathan Peppers <jonathan.peppers@gmail.com>
2016-02-19 14:38:50 -0600

    Build is working

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+using System.Collections;
+
+public class BuildScript 
+{
+    static void Android()
+    {
+		PlayerSettings.Android.keyaliasName = "StickmanEPIC";
+		PlayerSettings.Android.keyaliasPass =
+			PlayerSettings.Android.keystorePass = "hcents3@";
+		PlayerSettings.Android.keystoreName = Path.GetFullPath("Android.keystore").Replace('\\', '/');
+
+		if (!Directory.Exists ("build")) {
+			Directory.CreateDirectory ("build");
+		}
+
+		BuildPipeline.BuildPlayer(GetScenes(), "build/TerribleGame.apk", BuildTarget.Android, BuildOptions.None);
+    }
+
+    static string[] GetScenes()
+    {
+        return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
+    }
+}
