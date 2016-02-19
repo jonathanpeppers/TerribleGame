5c74b0b01ff71c7f4fe0e8d8f62d5e2f2357a2d2
Jonathan Peppers <jonathan.peppers@gmail.com>
2016-02-19 15:16:29 -0600

    iOS builds!

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -13,15 +13,26 @@ public class BuildScript
 			PlayerSettings.Android.keystorePass = "hcents3@";
 		PlayerSettings.Android.keystoreName = Path.GetFullPath("Android.keystore").Replace('\\', '/');
 
-		if (!Directory.Exists ("build")) {
-			Directory.CreateDirectory ("build");
-		}
+		CheckDir ("build");
 
 		BuildPipeline.BuildPlayer(GetScenes(), "build/TerribleGame.apk", BuildTarget.Android, BuildOptions.None);
     }
+
+	static void iOS()
+	{
+		CheckDir ("scratch");
+		PlayerSettings.SetScriptingDefineSymbolsForGroup (BuildTargetGroup.iOS, "IPHONE;BANANA;");
+		BuildPipeline.BuildPlayer(GetScenes(), "scratch", BuildTarget.iOS, BuildOptions.None);
+	}
 
     static string[] GetScenes()
     {
         return EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray();
     }
+
+	static void CheckDir(string dir)
+	{
+		if (!Directory.Exists(dir))
+			Directory.CreateDirectory(dir);
+	}
 }
--- build.fsx
+++ build.fsx
@@ -2,12 +2,25 @@
 open Fake
 open System
 
+let provisioningId = "8d6a320d-2ac8-438e-a47c-dcc5b51ad491"
+let provisioningName = "GenericInHouse"
+
 let Exec command args =
     let result = Shell.Exec(command, args)
     if result <> 0 then failwithf "%s exited with error %d" command result
 
 Target "android" (fun () ->
-  Exec "/Applications/Unity/Unity.app/Contents/MacOS/Unity" "-quit -batchmode -logFile -executeMethod BuildScript.Android"
+    Exec "/Applications/Unity/Unity.app/Contents/MacOS/Unity" "-quit -batchmode -logFile -executeMethod BuildScript.Android"
+)
+
+Target "ios-player" (fun () ->
+    Exec "/Applications/Unity/Unity.app/Contents/MacOS/Unity" "-quit -batchmode -logFile -executeMethod BuildScript.iOS"
+)
+
+Target "ios" (fun () ->
+    DeleteDir "scratch/TerribleGame.xarchive/"
+    Exec "xcodebuild" ("-project scratch/Unity-iPhone.xcodeproj -scheme Unity-iPhone archive -archivePath scratch/TerribleGame PROVISIONING_PROFILE=" + provisioningId)
+    Exec "xcodebuild" ("-exportArchive -archivePath scratch/TerribleGame.xcarchive -exportPath build/TerribleGame.ipa -exportProvisioningProfile " + provisioningName)
 )
 
 RunTarget()
